001f675c135b2321e288ea41bfbfe9b3ea488bc6
XxDTTE78xX <abrahamnunez7805@gmail.com>
2026-05-04 14:49:05 -0400

    Add instructions for Porta-lápices project

---
+++ Manualidades_1.py
@@ -0,0 +1,8 @@
+def ejecutar():
+    print("\n--- Proyecto: Porta-lápices Galáctico ---")
+    print("Dificultad: Facil | Duracion: Corta")
+    print("Materiales: Latas de refresco, pintura y pegamento.")
+    print("Instrucciones:")
+    print("1. Lava la lata y quita bordes filosos.")
+    print("2. Píntala con colores del FC Barcelona.")
+    print("3. ¡Listo!")
